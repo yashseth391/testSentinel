55361ef96735f394a3b2736a8c20394d79b9a2a2
Yash Seth <115554024+yashseth391@users.noreply.github.com>
2025-11-02 20:40:21 +0700

    Initial commit

---
+++ src/electron/main.ts
@@ -0,0 +1,18 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+
+
+// Needed when using ES modules
+type test = string
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+  if (isDev()) {
+    mainWindow.loadURL('http://localhost:5123');
+
+  } else {
+    const finalPath = path.join(app.getAppPath(), "/dist-react/index.html");
+    mainWindow.loadFile(finalPath);
+  }
+});
